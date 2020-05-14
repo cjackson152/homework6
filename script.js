7a368bfdcd8e27824ce9d8343fd8f37756e08d7b
Cole Jackson <cole_jackson@live.com>
2020-05-14 11:13:26 -0700

    further styling

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const openWeatherKey = "1e61f70bc517e0aeee95c1011a50a306";
-var currentForecast = $("#todaysWeather");
-var forecastHtml = $("#weekForecast");
+const openWeatherKey = '1e61f70bc517e0aeee95c1011a50a306';
+var currentForecast = $('#todaysWeather');
+var forecastHtml = $('#weekForecast');
 var city;
 
-$("#previousSearch").click(function() {
+$('#previousSearch').click(function() {
   let cityName = event.target.value;
   weather(cityName);
   forecast(cityName);
 })
 
 
-$("#submitCity").click(function() {
+$('#submitCity').click(function() {
   event.preventDefault();
-  let cityName = $("#cityInput").val();
+  let cityName = $('#cityInput').val();
   weather(cityName);
   forecast(cityName);
 });
@@ -24,18 +24,18 @@ function forecast(cityName) {
         let forecastInfo = response.list;
         forecastHtml.empty();
         $.each(forecastInfo, function(i) {
-            if (!forecastInfo[i].dt_txt.includes("12:00:00")) {
+            if (!forecastInfo[i].dt_txt.includes('12:00:00')) {
                 return;
             }
             let forecastDate = new Date(forecastInfo[i].dt*1000);
             let weatherIcon = `https://openweathermap.org/img/wn/${forecastInfo[i].weather[0].icon}.png`;
 
             forecastHtml.append(
-              `<div class="col-md">
-                <div class="card text-white bg-success">
-                    <div class="card-body">
+              `<div class='col-md'>
+                <div class='card text-white bg-success'>
+                    <div class='card-body'>
                         <h4 class='card-title'>${forecastDate.getMonth()+1}/${forecastDate.getDate()}/${forecastDate.getFullYear()}</h4>
-                        <img src=${weatherIcon} alt="Icon">
+                        <img src=${weatherIcon} alt='Icon'>
                         <p>Temperature: ${forecastInfo[i].main.temp} &#176;F</p>
                         <p>Humidity: ${forecastInfo[i].main.humidity}%</p>
                     </div>
@@ -52,20 +52,20 @@ function uvi(coordinates) {
 
   $.get(queryURL).then(function(response){
       let currentUVI = response.value;
-      let uviSeverity = "green";
-      let textColour = "white"
+      let uviSeverity = 'green';
+      let textColour = 'white'
       if (currentUVI >= 11) {
-          uviSeverity = "purple";
+          uviSeverity = 'purple';
       } else if (currentUVI >= 8) {
-          uviSeverity = "red";
+          uviSeverity = 'red';
       } else if (currentUVI >= 6) {
-          uviSeverity = "orange";
-          textColour = "black"
+          uviSeverity = 'orange';
+          textColour = 'black'
       } else if (currentUVI >= 3) {
-          uviSeverity = "yellow";
-          textColour = "black"
+          uviSeverity = 'yellow';
+          textColour = 'black'
       }
-      currentForecast.append(`<p>UV Index: <span class="text-${textColour} uvPadding" style="background-color: ${uviSeverity};">${currentUVI}</span></p>`);
+      currentForecast.append(`<p>UV Index: <span class='text-${textColour} uvPadding' style='background-color: ${uviSeverity};'>${currentUVI}</span></p>`);
   })
 }
 
@@ -80,10 +80,10 @@ function weather(cityName) {
 
       currentForecast.html(
         `
-      <h2>${response.name}, ${response.sys.country} (${time.getMonth()+1}/${time.getDate()}/${time.getFullYear()})<img src=${weatherIcon} height="70px"></h2>
+      <h2>${response.name}, ${response.sys.country} (${time.getMonth()+1}/${time.getDate()}/${time.getFullYear()})<img src=${weatherIcon} height='80px'></h2>
       <p>Current Temperature: ${response.main.temp} &#176;F</p>
       <p>Current Humidity: ${response.main.humidity}%</p>
-      <p>Current Wind Speed: ${response.wind.speed} m/s</p>
+      <p>Current Wind Speed: ${response.wind.speed} MPH</p>
       `,
       uvi(response.coord))
       previousSearch(response.name);
@@ -99,11 +99,11 @@ function previousSearch(cityName) {
     
     if (!city.includes(cityName)){
         city.push(cityName);
-        localStorage.setItem("weatherSearch", JSON.stringify(city));
+        localStorage.setItem('weatherSearch', JSON.stringify(city));
     }
 
-    $("#previousSearch").prepend(`
-    <button class="btn btn-dark cityHistoryBtn" value='${cityName}'>${cityName}</button>
+    $('#previousSearch').prepend(`
+    <button class='btn btn-dark cityHistoryBtn' value='${cityName}'>${cityName}</button>
     `);
 }
 
@@ -113,14 +113,14 @@ function searchHistory(array) {
     })
 }
 
-if (localStorage.getItem("weatherSearch")) {
-  city = JSON.parse(localStorage.getItem("weatherSearch"));
+if (localStorage.getItem('weatherSearch')) {
+  city = JSON.parse(localStorage.getItem('weatherSearch'));
   searchHistory(city);
 } else {
   city = [];
 };
 
 
-weather("Newport Beach");
-forecast("Newport Beach");
+weather('Irvine, CA');
+forecast('Irvine, CA');
 
